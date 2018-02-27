fc585d62a34e5b7aff0934c05ffb6245eb49954a
Visarut Phusua <aof.dev@gmail.com>
2018-02-27 21:18:11 +0700

    Lazy load in components :pizza:

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import App from './App'
+const App = () => import('./App')
 import router from './router'
 Vue.use(Vuetify)
 require('../node_modules/vuetify/dist/vuetify.min.css')
